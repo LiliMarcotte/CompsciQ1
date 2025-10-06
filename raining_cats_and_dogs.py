8f1a59602c3e34ab77a3f1c1785d22dca70555f5
LiliMarcotte <liliisawesome06@gmail.com>
2025-10-05 20:44:15 -0400

    Add files via upload

---
+++ raining_cats_and_dogs.py
@@ -0,0 +1,12 @@
+# Program: Raining Cats and Dogs
+# This program prints "It is raining cats and dogs."
+
+# Set variables
+c = "cats."
+d = "dogs."
+
+# Set variable s to the full sentence
+s = "It is raining " + c[:-1] + " and " + d
+
+# Print the sentence
+print(s)
